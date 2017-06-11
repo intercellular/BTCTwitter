f45e2b3ca720b6193e42fc3ab6b3d5130d315f8e
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-06-11 14:35:52 -0400

    More

--- btc.js
+++ btc.js
@@ -1,18 +1,54 @@
-ಠᴥಠ = { 
-  $components: [
-    { $components: [], class: "container",
-      _add: function(data){
-        this.$components = [this._item(data)].concat(this.$components).slice(0,50) },
-      $init: function(){
-        this._ws = new WebSocket('wss://twitsocket.herokuapp.com').addEventListener('message', function (event) { this._add(JSON.parse(event.data)) }.bind(this)) },
-      _item: function(data){
-        return {
-          class: "item hidden",
-          $init: function(){ this.class = "item" },
-          $components: [
-            { style: "display: block;", $components: [ {$type: "img", class: "avatar", src: data.user.profile_image_url_https } ] },
-            { class: "body",
-              $components: [
-                { $type: "h5", $text: data.user.name + " (" + data.user.screen_name + ")" },
-                { $type: "text", $text: data.text }
-              ].concat( data.extended_entities && data.extended_entities.media ? [{ $type: "img", src: data.extended_entities.media[0].media_url_https }] : [] ) } ] } } }], $cell: true }
+Stream = {
+  id: "stream",
+  $cell: true,
+  class: "container",
+  _timestamps: function(){ return this.querySelectorAll(".timestamp") },
+  $init: function(){
+    this._bitcoin = Bitcoin(this);
+  },
+  _add: function(data){
+    console.log("data", data);
+    if(data[1] !== "hb"){
+      this.$components.unshift(Item({dollars: data[1], timestamp: Date.now()}))
+      this._timestamps().forEach(function(timestamp){ timestamp._refresh() })
+    }
+  },
+  $components: []
+};
+Item = function(o){
+  return {
+    class: "row hidden",
+    $init: function(){
+      var t = this;
+      setTimeout(function(){ t._display() }, 200)
+    },
+    _display: function(){
+      this.class = "row";
+    },
+    $components: [
+      {
+        $type: "h1",
+        $text: "$" + o.dollars
+      },
+      {
+        class: "timestamp",
+        _timestamp: o.timestamp,
+        _refresh: function(){ this.$text = Timeago(this._timestamp) },
+        $text: Timeago(o.timestamp)
+      }
+    ]
+  }
+};
+Bitcoin = function($el){
+  var ws = new WebSocket('wss://api.bitfinex.com/ws')
+  ws.addEventListener('message', function (event) {
+    if(Array.isArray(JSON.parse(event.data))) $el._add(JSON.parse(event.data))
+  })
+  ws.addEventListener('open', function (event) {
+    ws.send(JSON.stringify({ "event":"subscribe", "channel":"ticker", "pair":"BTCUSD" }))
+  })
+  return ws;
+};
+Timeago = function(d){
+  return timeago().format(d);
+};
